e9ee53b5d0e167aa2e756118f6a175b46355a324
Mohamed <mohamedhelmoghazy.gmail.com>
2020-08-15 16:48:45 -0600

    Add functionality of UpSert

--- BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/Controllers/BooksController.cs
@@ -2,15 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListMVC.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty] // used becuase we do not want to retrive the book entry every time so we binded book with the database
+        public Book Book { get; set; }
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        // The upsert will be used in two case either to insert new entry so no id requried or edit so the ID is not required , 
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            if (id==null)
+            {
+                // create request
+                return View(Book);
+
+            }
+            // update case
+            Book = _db.Books.FirstOrDefault(u => u.Id == id);
+            if (Book==null)
+            {
+                return NotFound();
+
+            }
+            return View(Book);
+        }
+
+
+        #region API Calls
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+
+
+        #endregion
     }
 }
