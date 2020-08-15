26534dbce1beaa025f346c7fe20a1219e482150c
Mohamed <mohamedhelmoghazy.gmail.com>
2020-08-15 14:47:31 -0600

    Create and LinkDB

---
+++ BookListMVC/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class Book
+    {
+        [Key] // data annotation to set the key 
+        public int Id { get; set; }
+        [Required] // data annotation to mark the name filed is required
+        public String Name { get; set; }
+
+        public String Author { get; set; }
+        public String ISBN { get; set; }
+
+
+
+    }
+}
